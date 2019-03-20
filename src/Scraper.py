4eae6ce7535b5e27e433ba6b10171674514a3e8e
Dominik Buszowiecki <buszowid@mcmaster.ca>
2019-03-19 22:58:03 -0400

    Algorithm now find Body Style

--- src/Scraper.py
+++ src/Scraper.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import re
 
 def getCar(url:str):
-    categories = ['Price']
+    categories = ['Price', 'Body Style']
     values = []
     # Connect to the URL
     response = requests.get(url)
@@ -14,20 +14,23 @@ def getCar(url:str):
     # Parse HTML and save to BeautifulSoup object¶
     soup = BeautifulSoup(response.text, "html.parser")
     carSoup = soup.findAll("script", {"type": "text/javascript"})
-    car = str(carSoup[0]).split("\n")[3]
-    print(car)
-    car = car.split(',')[]
-    for i in car:
-        print(i)
 
-    # #Scrapes the price of the vehicle
-    # price = str(soup.findAll("span", class_="vehicle-info__price-display"))[44:].strip("</span>]")
-    # values.append(price)
-    #
-    # #Extract each category and their value
-    # for car in carSoup:
-    #     car = str(car)[46:-13].split(":</strong><span> ")
-    #     categories.append(car[0])
-    #     values.append(car[1])
+    #Finds the body Style of a particular vehicle
+    body = str(carSoup[0]).split("\n")[3]
+    m = re.search('"bodyStyle":"(.+?)"', body)
+    if m:
+        cat = m.group(1)
+
+    #Scrapes the price of the vehicle
+    price = str(soup.findAll("span", class_="vehicle-info__price-display"))[44:].strip("</span>]")
+    values.append(price)
+    values.append(cat)
+
+    #Extract each category and their value
+    carSoup = soup.findAll("li", {"class": "vdp-details-basics__item"})
+    for car in carSoup:
+        car = str(car)[46:-13].split(":</strong><span> ")
+        categories.append(car[0])
+        values.append(car[1])
 
     return categories, values
--- src/testClient.py
+++ src/testClient.py
@@ -3,3 +3,5 @@ from Scraper import getCar
 # Set the URL you want to webscrape from
 url = 'https://www.cars.com/vehicledetail/detail/746043557/overview/'
 cat, value = getCar(url)
+for i in range(len(cat)):
+    print(cat[i],"-", value[i])
