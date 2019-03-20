12c9f8f4f7ca14f8c0ad0b4996c02bef4c5ddfc5
Dominik Buszowiecki <buszowid@mcmaster.ca>
2019-03-19 22:45:12 -0400

    Implementation updated

--- src/Scraper.py
+++ src/Scraper.py
@@ -3,43 +3,31 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+import re
 
 def getCar(url:str):
-    categories = ['price']
+    categories = ['Price']
     values = []
     # Connect to the URL
     response = requests.get(url)
 
     # Parse HTML and save to BeautifulSoup object¶
     soup = BeautifulSoup(response.text, "html.parser")
-    carSoup = soup.findAll('li', class_="vdp-details-basics__item")
-    price = str(soup.findAll("span",  ))[44:].strip("</span>]")
-    values.append(price)
-    for car in carSoup:
-        print(str(car)[30:])
+    carSoup = soup.findAll("script", {"type": "text/javascript"})
+    car = str(carSoup[0]).split("\n")[3]
+    print(car)
+    car = car.split(',')[]
+    for i in car:
+        print(i)
 
-#Comment
+    # #Scrapes the price of the vehicle
+    # price = str(soup.findAll("span", class_="vehicle-info__price-display"))[44:].strip("</span>]")
+    # values.append(price)
+    #
+    # #Extract each category and their value
+    # for car in carSoup:
+    #     car = str(car)[46:-13].split(":</strong><span> ")
+    #     categories.append(car[0])
+    #     values.append(car[1])
 
-    print(carSoup)
-
-
-# Set the URL you want to webscrape from
-url = 'https://www.cars.com/vehicledetail/detail/746043557/overview/'
-getCar(url)
-
-# for i in range(len(titles) - 1):
-#     car.append(str(carSoup[i]).split("<span> ")[-1].strip("</span>\n</li>"))e
-
-# catSoup = soup.findAll('strong')
-# carSoup = soup.findAll('span')
-
-
-# car = str(carSoup).split('</span>, ')[30:38]
-# category = str(catSoup).split('</strong>')[0:8]
-# category[0] = category[0][9:19]
-# car[0] = car[0][7:]
-#
-# # for i in range(1,len(category)):
-# #     category[i] = category[i][10:]
-# #     car[i] = car[i][7:]
-# #     print(category[i], car[i])
+    return categories, values
---
+++ src/testClient.py
@@ -0,0 +1,5 @@
+from Scraper import getCar
+
+# Set the URL you want to webscrape from
+url = 'https://www.cars.com/vehicledetail/detail/746043557/overview/'
+cat, value = getCar(url)
