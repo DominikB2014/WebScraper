a733f0ed0e7d8f8e8e5960a47d08dd1e72723a8c
Dominik Buszowiecki <buszowid@mcmaster.ca>
2019-03-21 13:19:32 -0400

    Added import to constant file

--- src/Write.py
+++ src/Write.py
@@ -1,12 +1,11 @@
 import csv
 
-categories = ['Body Style', 'Fuel Type', 'City MPG', 'Highway MPG', 'DriveTrain',
-              'Transmission', 'Engine']
+from Constants import CATEGORIES
 file_name = "cars.csv"    # Name of file
 
 
-# Resets/Creates the file
 def init_data():
+    """Resets/Sets the data"""
     with open('../Data/' + file_name, mode='w', newline='') as cars_file:
         car_writer = csv.writer(cars_file, delimiter=',', quotechar='"',
                                      quoting=csv.QUOTE_MINIMAL)
@@ -14,22 +13,22 @@ def init_data():
         car_writer.writerow(['Make', 'Model', 'Year', 'Price'] + categories)
 
 
-# Adds a car to the databse
-def write_car(make:str, model:str , year: str, values):
+def write_car(make: str, model: str , year: str, values):
+    """Adds a car to the database"""
     with open('../Data/cars.csv', mode='a', newline='') as cars_file:
         car_writer = csv.writer(cars_file, delimiter=',', quotechar='"',
                                      quoting=csv.QUOTE_MINIMAL)
         car_writer.writerow([make, model, year] + filter_car(values))
 
 
-# Filters out un-needed categories
 def filter_car(values):
+    """Filters out un-needed categories"""
     new_car = []
     i, j = 0, 0
     while i < 7:
         j = i
         while j < len(values):
-            if categories[i] == values[j][0]:
+            if CATEGORIES[i] == values[j][0]:
                 new_car.append(values[j][1])
                 i += 1
                 break
