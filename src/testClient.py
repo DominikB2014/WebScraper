e720d8d54716689fb7687e5245779c08bc38b038
Dominik Buszowiecki <buszowid@mcmaster.ca>
2019-03-21 19:17:37 -0400

    Client updated

--- src/testClient.py
+++ src/testClient.py
@@ -1,14 +1,21 @@
 from Scraper import *
+from Constants import MAKES
 import Write
+import multiprocessing
+from enum import Enum
 
 # Set the URL you want to webscrape from
 url = 'https://www.cars.com/vehicledetail/detail/752964006/overview/'
 url2 = 'https://www.cars.com/vehicledetail/detail/766847902/overview/'
-url3 = 'https://www.cars.com/vehicledetail/detail/759929739/overview/'
 url4 = 'https://www.cars.com/vehicledetail/detail/757712014/overview/'
 Write.init_data()
-Write.write_car("Cheverolet", "Colorado", "2019", getCar(url))
-Write.write_car("Buggatti","Veyron", "2018", getCar(url2))
-Write.write_car("Audi", "RS5", "2019", getCar(url3))
-Write.write_car("Dodge", "Challenger", "2018", getCar(url4))
+Write.write_car("Cheverolet", "Colorado", "2019", get_car(url))
+Write.write_car("Buggatti","Veyron", "2018", get_car(url2))
+Write.write_car("Dodge", "Challenger", "2018", get_car(url4))
 
+#
+# make = MAKES[4]  # Acura
+# model = "47843"
+# year = "36362520"
+#
+# print(get_ca)
