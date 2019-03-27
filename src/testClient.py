6e54759c1d799e3ffdc166ccd4c125c430d7be4d
Dominik Buszowiecki <buszowid@mcmaster.ca>
2019-03-27 15:49:26 -0400

    Added file to all possible year id's

--- src/testClient.py
+++ src/testClient.py
@@ -4,18 +4,14 @@ import Write
 import multiprocessing
 from enum import Enum
 
-# Set the URL you want to webscrape from
+# # Set the URL you want to webscrape from
 url = 'https://www.cars.com/vehicledetail/detail/752964006/overview/'
 url2 = 'https://www.cars.com/vehicledetail/detail/766847902/overview/'
 url4 = 'https://www.cars.com/vehicledetail/detail/757712014/overview/'
-Write.init_data()
-Write.write_car("Cheverolet", "Colorado", "2019", get_car(url))
-Write.write_car("Buggatti","Veyron", "2018", get_car(url2))
-Write.write_car("Dodge", "Challenger", "2018", get_car(url4))
+# Write.init_data()
+# Write.write_car("Cheverolet", "Colorado", "2019", get_car(url))
+# Write.write_car("Buggatti","Veyron", "2018", get_car(url2))
+# Write.write_car("Dodge", "Challenger", "2018", get_car(url4))
 
+get_year_ids()
 #
-# make = MAKES[4]  # Acura
-# model = "47843"
-# year = "36362520"
-#
-# print(get_ca)
